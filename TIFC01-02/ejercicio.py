21d0a6e9fbfe8136046919c1dc56464f77d1dfb0
bubu-laptop <luisangel@ciencias.unam.mx>
2026-07-10 13:08:51 -0600

    Subiendo primer ejercicio de python

---
+++ TIFC01-02/ejercicio.py
@@ -0,0 +1,29 @@
+print("Introduce el nombre:")
+nombre = input()
+print(nombre + " started their first Generation course today.")
+
+print("Introduce el trabajo:")
+trabajo = input()
+print("They are training as a " + trabajo + ".")
+
+print("Introduce el primer adjetivo:")
+adjetivo1 = input()
+print("Introduce el segundo adjetivo:")
+adjetivo2 = input()
+print("They found their cohort to be very " + adjetivo1 + " but their teacher was, at least, " + adjetivo2 + ".")
+
+print("Introduce la primera comida:")
+comida1 = input()
+print("Introduce la segunda comida:")
+comida2 = input()
+print("For lunch they have " + comida1 + " and " + comida2 + " while reviewing their notes.")
+
+print("Introduce la emoción:")
+emocion = input()
+print("They feel " + emocion + " but are determined to complete the course.")
+
+print("--- RESULTADO FINAL DE TODO ---")
+
+historia = nombre + " started their first Generation course today. They are training as a " + trabajo + ". They found their cohort to be very " + adjetivo1 + " but their teacher was, at least, " + adjetivo2 + ". For lunch they have " + comida1 + " and " + comida2 + " while reviewing their notes. They feel " + emocion + " but are determined to complete the course."
+
+print(historia) 
